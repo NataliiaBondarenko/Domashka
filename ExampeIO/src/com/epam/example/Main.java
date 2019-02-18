d12a9f297a1103e215aeeb8d25fe8473b60b7c87
NataliiaBondarenko <5922597@gmail.com>
2019-02-18 23:00:33 +0200

    My first  commit

---
+++ ExampeIO/src/com/epam/example/Main.java
@@ -0,0 +1,30 @@
+package com.epam.example;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("* * * *   * * * *        *         *       *");
+        System.out.println("*         *     *      *   *       *  * *  *");
+        System.out.println("* *       * * * *     *     *      *   *   *");
+        System.out.println("*         *          * * * * *     *       *");
+        System.out.println("* * * *   *         *         *    *       *");
+
+
+
+        System.out.println("* * * *   * * * *        *         *       *");
+        System.out.println("                                            ");
+        System.out.println("*         *     *      *   *       *  * *  *");
+        System.out.println("                                            ");
+        System.out.println("* *       * * * *     *     *      *   *   *");
+        System.out.println("                                            ");
+        System.out.println("*         *          * * * * *     *       *");
+        System.out.println("                                            ");
+        System.out.println("* * * *   *         *         *    *       *");
+
+
+
+    }
+}
