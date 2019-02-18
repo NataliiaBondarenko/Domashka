3df11068c3b65869cf952ef95b27ff86bc865e6e
NataliiaBondarenko <5922597@gmail.com>
2019-02-18 23:03:58 +0200

    Remove whitespaces

--- ExampeIO/src/com/epam/example/Main.java
+++ ExampeIO/src/com/epam/example/Main.java
@@ -11,9 +11,6 @@ public class Main {
         System.out.println("* *       * * * *     *     *      *   *   *");
         System.out.println("*         *          * * * * *     *       *");
         System.out.println("* * * *   *         *         *    *       *");
-
-
-
         System.out.println("* * * *   * * * *        *         *       *");
         System.out.println("                                            ");
         System.out.println("*         *     *      *   *       *  * *  *");
